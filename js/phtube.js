7bfd43d987d69fe6b2cd9d04204667aaa567274b
ahmisrafil <ahmisrafil@yahoo.com>
2023-09-03 01:32:52 +0600

    category added

--- js/phtube.js
+++ js/phtube.js
@@ -1,1 +1,18 @@
-console.log('welcome')
+// load category from api 
+const handleCategory = async () => {
+    const response = await fetch('https://openapi.programming-hero.com/api/videos/categories');
+    const data = await response.json();
+
+    const categoryContainer = document.getElementById('category-container');
+    data.data.forEach((category) => {
+    const div = document.createElement('div');
+    div.innerHTML= `
+    <a class="tab btn btn-active">${category.category}</a> 
+    `    
+    categoryContainer.appendChild(div);
+    });
+    console.log(data.data[0].category);
+}
+
+ 
+handleCategory();
