33c328f70c48c561d2fd4dafe3ddadcd8d88db12
ahmisrafil <ahmisrafil@yahoo.com>
2023-09-04 02:05:54 +0600

    card updated

--- js/phtube.js
+++ js/phtube.js
@@ -15,14 +15,37 @@ const handleCategory = async () => {
     });
 
 }
+
 // data load based on category
 
 const handleClick= async(categoryId)=>{
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
-
     const data = await response.json();
-    
-    console.log(data.data);
+
+    const cardContainer = document.getElementById('card-container');
+
+    data.data.forEach((card) => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div class="card w-48 bg-base-100 shadow-xl">
+        <figure><img src="${card.thumbnail}" alt="Shoes" /></figure>
+        <div class="card-body flex flex-row">
+            <div><img src="${card.authors[0].profile_picture}" alt="profile" class="h-10 w-10 rounded-full"></div>
+            <div>
+                <h2 class="card-title">
+                    Shoes!
+                  </h2>
+                  <p class="text-sm font-normal text-[#171717] opacity-70">${card.authors[0].profile_name}</p>
+                  <p class="text-sm font-normal text-[#171717] opacity-70">${card.others.views} views</p>        
+            </div>
+        </div>
+        </div>
+        `
+        console.log(card);
+        cardContainer.appendChild(div);
+    });
+
+    // console.log(data.data[0].authors[0].profile_picture);
 }
 
  
