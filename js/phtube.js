c54be67b49780f192aa7ec0323fbe8183e40d6f7
ahmisrafil <ahmisrafil@yahoo.com>
2023-09-04 04:22:48 +0600

    time and error msg added

--- js/phtube.js
+++ js/phtube.js
@@ -10,7 +10,6 @@ const handleCategory = async () => {
     <a onclick="handleClick('${category.category_id}')" class="tab btn btn-active ">${category.category}</a> 
     `    
     categoryContainer.appendChild(div);
-    console.log(category);
 
     });
 
@@ -21,47 +20,80 @@ const handleCategory = async () => {
 const handleClick= async(categoryId)=>{
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
     const data = await response.json();
-
     const cardContainer = document.getElementById('card-container');
-
-    data.data.forEach((card) => {
+    cardContainer.innerHTML = '';
+    console.log(categoryId);
+    if(categoryId === '1005'){
         const div = document.createElement('div');
         div.innerHTML = `
-        <div class="card w-60 h-80 bg-base-100 shadow-xl">
-        <figure><img src="${card.thumbnail}" alt="Shoes" /></figure>
-        <div class="bg-black text-white">
-            <p id="time"></p>
+        <div class="text-center">
+        <img src="Icon.png">
+        <h3>Oops! Sorry. There is no content here
+        </h3>
+        </div>
+       
+
+        `
+        cardContainer.appendChild(div);
+        }
+   
+    data.data.forEach((card) => {
+        
+        const div = document.createElement('div');
+        const postedDate = card.others.posted_date;
+        const time = convertSeconds(postedDate);
+
+        div.innerHTML = `
+        <div class="card  h-80 bg-base-100 shadow-xl">
+        <figure><img src="${card?.thumbnail}" alt="Shoes" /></figure>
+        <div class="bg-black text-white w-1/2 -mt-12 ">
+            <p id="time">${time.hours} hrs ${time.minutes} min ago</p>
         </div>
         <div class="card-body flex flex-row">
-            <div><img src="${card.authors[0].profile_picture}" alt="profile" class="h-10 w-10 rounded-full"></div>
+            <div><img src="${card?.authors[0].profile_picture}" alt="profile" class="h-10 w-10 rounded-full"></div>
             <div>
                 <h2 class="card-title">
-                    ${card.title}
+                    ${card?.title}
                   </h2>
-                  <p class="text-sm font-normal text-[#171717] opacity-70">${card.authors[0].profile_name}</p>
-                  <p class="text-sm font-normal text-[#171717] opacity-70">${card.others.views} views</p>        
+                  <p class="text-sm font-normal text-[#171717] opacity-70">${card?.authors[0]?.profile_name}</p>
+                  <p class="text-sm font-normal text-[#171717] opacity-70">${card?.others?.views} views</p>        
             </div>
         </div>
         </div>
         `
+        if(card.length===0){
+            console.log("oops! sorry");
+        }
         // console.log(card.others.posted_date);
 
         // // convert time 
-        // const postedDate = card.others.posted_date;
-        // function convertSeconds(seconds) {
-        //     const hours = Math.floor(seconds / 3600);
-        //     const minutes = Math.floor((seconds % 3600) / 60);
-        //     console.log(hours +' hours'+ minutes +' minutes ago')
-        //   }
-        // //   const time = document.getElementById('time');
+        
+        function convertSeconds(seconds) {
+            
+            const hours = Math.floor(seconds / 3600);
+            const minutes = Math.floor((seconds % 3600) / 60);
+            return{
+                hours, minutes,
+            } ;
+            
+          }
+          
+        //   const time = document.getElementById('time');
         //   const timeText = convertSeconds(postedDate);
         //   document.getElementById('').textContent = timeText;
 
 
         cardContainer.appendChild(div);
     });
+   
 }
 
+const handleSort = async() => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
+    const data = await response.json();
+
+}
+    
  
 handleCategory();
 handleClick("1000")
